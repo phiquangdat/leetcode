0be347edf50304da3c16e75aafea9277ea09ac41
Dat Phi <88042242+phiquangdat@users.noreply.github.com>
2025-03-03 01:48:20 +0200

    Time: 1442 ms (10.82%) | Memory: 0B (100.00%) - LeetSync

--- 1908-recyclable-and-low-fat-products/recyclable-and-low-fat-products.sql
+++ 1908-recyclable-and-low-fat-products/recyclable-and-low-fat-products.sql
@@ -1,4 +1,6 @@
-# Write your MySQL query statement below
-SELECT product_id FROM products 
-WHERE low_fats='Y' AND recyclable = 'Y' 
-ORDER BY product_id;
+SELECT
+    product_id
+FROM
+    Products
+WHERE
+    low_fats = 'Y' AND recyclable = 'Y'
