23e14968b72d0322973464faa8f41c5db30230ff
Dat Phi <88042242+phiquangdat@users.noreply.github.com>
2025-03-09 17:48:41 +0200

    Time: 0 ms (100.00%) | Memory: 53.9 MB (8.59%) - LeetSync

---
+++ 27-remove-element/remove-element.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+    let count = 0;
+    for(let i = 0; i < nums.length; i++){
+        if(nums[i] !== val){
+            nums[count] = nums[i];
+            count++;
+        }
+    }
+    return count;
+};
+
+
+nums = [3,2,2,3], val = 3
+
+console.log(removeElement(nums, val));
+console.log(nums);
+
+
