70d1e9ee12893ca4cdf15148714710d151f4d58a
Dat Phi <88042242+phiquangdat@users.noreply.github.com>
2025-03-16 23:31:09 +0200

    Time: 0 ms (100.00%) | Memory: 55.4 MB (13.15%) - LeetSync

--- 67-add-binary/add-binary.js
+++ 67-add-binary/add-binary.js
@@ -4,17 +4,14 @@
  * @return {string}
  */
 var addBinary = function(a, b) {
-    // Handle empty strings
     if(a === "" || b === "") {
         return a || b;
     }
     
-    // Handle "0" strings
     if(a === "0" && b === "0") {
         return "0";
     }
     
-    // Convert binary strings to BigInt
     const num1 = BigInt('0b' + a);
     const num2 = BigInt('0b' + b);
     
